77a641a3f032ae10bebeaac10a336a259b4443eb
Filip Jansson <48802078+FJ05@users.noreply.github.com>
2024-11-05 18:06:46 +0100

    Squashed commit of the following:
    commit a4a8f301a048e967d152f1de2516de1135fe209f
    Merge: 4943d39 fbffc7e
    Author: Filip Jansson <48802078+FJ05@users.noreply.github.com>
    Date:   Tue Nov 5 18:04:38 2024 +0100
    
        Merge branch 'wilhelm'
    
    commit 4943d395cbc9af8e82207fa472801741ef8aa7cc
    Merge: 0d9a882 593534a
    Author: Schmats <maxlovheim@gmail.com>
    Date:   Mon Nov 4 16:43:19 2024 +0100
    
        Merge branch 'main' of https://github.com/FJ05/projectRL
    
    commit 0d9a8828c615ca70d6a768887972f4c44b1f3d79
    Author: Schmats <maxlovheim@gmail.com>
    Date:   Mon Nov 4 16:43:04 2024 +0100
    
        Testar

--- engine/eventHandlers/inputHandlers/clickInputHandler.py
+++ engine/eventHandlers/inputHandlers/clickInputHandler.py
@@ -1,27 +1,21 @@
 import pygame
-import math
-class ClickInputHandler():
+# this handler support wasd movement.
+class InputHandler:
 
-    def __init__(self, call_back_function, object=None) -> None:
+    def __init__(self, call_back_function, entity_object=None) -> None:
         self.call_back = call_back_function
-        self.object = object
-        self.pressed = False
+        self.entity = entity_object
 
     def process_input(self):
-        if self.object is None:
+        if self.entity is None:
             return
+
         mouse_buttons = pygame.mouse.get_pressed()
-
-        if mouse_buttons[0] and self.pressed == False:  # Left mouse button
-            self.pressed = True
+        if mouse_buttons[0]:  # Left mouse button
             mouse_pos = pygame.mouse.get_pos()
-            dy = mouse_pos[1] - self.object.get_y()
-            dx = mouse_pos[0] - self.object.get_x()
-
-            # Calculate the angle to mouse, adjusted by 90 degrees
-            angle = math.atan2(dy, dx)
-            self.call_back(angle)
-        elif mouse_buttons[0] == False and self.pressed == True:
-            self.pressed = False
-
+            print("Rect:",self.entity.get_rect())
+            print("Pos: ",self.entity.pos)
+            if self.entity.rect.collidepoint(mouse_pos):  # Check if click is within rect bounds
+                print("Clicked start")
+                self.call_back()
 
