621beecc07e93225d4e037738c2c55573d62945c
Wi1helm <wilhelmlidblad@gmail.com>
2024-11-05 14:32:39 +0100

    Fixed scaling for arrows

--- objects/entities/arrow.py
+++ objects/entities/arrow.py
@@ -10,10 +10,12 @@ class Arrow(EntityObject):
         self.velMax = [7, 7]  # Maximum velocity
         self.angle = angle
         self.rect = (20, 20)
-
+        
+        rect = pygame.display.get_window_size()
         image_path = "assets/player/player.png"
 
         self.surface = pygame.image.load(image_path).convert_alpha()
+        self.surface = pygame.transform.scale(self.surface, (rect[0] / 16, rect[1] / 9))
 
     def update_movement(self):
         self.pos = self.get_x() + math.cos(self.angle) * self.getMaxVelX(), self.get_y() + math.sin(self.angle) * self.getMaxVelY()
