e112c3bcf2c1a2ff8142c2d46342842efb6eade7
Schmats <83768694+Schmats@users.noreply.github.com>
2024-11-06 15:50:03 +0100

    Shorten slimeclass
    added comments and removed unused variables

--- objects/entities/blue_slime.py
+++ objects/entities/blue_slime.py
@@ -8,18 +8,15 @@ class Blue_Slime(EntityObject):
     def __init__(self, level: int, pos: tuple):
         super().__init__(level, pos)
         self.velMax = [2, 2]  # Maximum velocity
-        self.colliding = False
-        self.last_dir = (0,0)
-        # sets the path to the player
         self.image_path = "assets/player/player.png"
         rect = pygame.display.get_window_size()
         self.add_tag("Enemy")
 
-        # load the player with given player asset
+        # load the slime with given enemy asset
         self.surface =  pygame.image.load(self.image_path).convert_alpha()
         self.surface = pygame.transform.scale(self.surface, (rect[0] / 16, rect[1] / 9))
 
-    def update_movement(self, player_pos):
+    def update_movement(self, player_pos): # Updates movement of slime as a vector towards player position
         
        # Find direction vector (dx, dy) between enemy and player.
         dirvect = pygame.math.Vector2(player_pos[0] - self.get_x(),
